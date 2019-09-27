8d28fd0db0a754626c0a43e5376ace43f252efa4
Alynx Zhou <alynx.zhou@gmail.com>
2019-09-27 21:43:05 +0800

    Updated to support splited json.

--- README.md
+++ README.md
@@ -11,8 +11,11 @@ First go to your site dir and add following to your `config.yml`.
 ```yaml
 search:
   enable: true
-  path: search.json
-  page: search.html
+  # Use array to split contents.
+  path:
+    - search/1.json
+    - search/2.json
+  page: search/index.html
 ```
 
 then run this command to install it.
--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 module.exports = (hikaru) => {
-	const {getPathFn} = hikaru.utils
+	const {getPathFn, isString} = hikaru.utils
 	const {File} = hikaru.types
-	hikaru.generator.register("searching result page", (site) => {
+	hikaru.generator.register('searching result page', (site) => {
 		if (!site['siteConfig']['search']['enable']) {
 			return
 		}
 		return new File({
 			'docDir': site['siteConfig']['docDir'],
-			'docPath': site['siteConfig']['search']['page'] || 'search.html',
+			'docPath': site['siteConfig']['search']['page'] || 'search/index.html',
 			'title': 'search',
-			'layout': 'search'
+			'layout': 'search',
 		})
 	})
 	hikaru.generator.register('searching index json', (site) => {
 		if (!site['siteConfig']['search']['enable']) {
 			return
 		}
-		const search = []
-		const all = site['pages'].concat(site['posts'])
 		const getPath = getPathFn(site['siteConfig']['rootDir'])
-		for (let p of all) {
-			// Prefer to remove tags from HTML content.
-			search.push({
-				'title': `${p['title']}`,
-				'url': getPath(p['docPath']),
-				'content': p['content'].replace(/<\/?[^>]+>/gi, '')
-			})
+		let path = site['siteConfig']['search']['path']
+		if (isString(path)) {
+			path = [path]
 		}
-		return new File({
-			'docDir': site['siteConfig']['docDir'],
-			'docPath': site['siteConfig']['search']['path'] || 'search.json',
-			'content': JSON.stringify(search)
-		})
+		const all = site['pages'].concat(site['posts'])
+		const length = Math.round(all.length / path.length)
+		const result = []
+		for (let i = 0; i < path.length; ++i) {
+			const search = {'data': []}
+			const current = all.slice(i * length, (i + 1) * length)
+			for (let p of current) {
+				// Prefer to remove tags from HTML content.
+				search['data'].push({
+					'title': `${p['title']}`,
+					'url': getPath(p['docPath']),
+					'content': p['content'].replace(/<\/?[^>]+>/gi, '')
+				})
+			}
+			result.push(new File({
+				'docDir': site['siteConfig']['docDir'],
+				'docPath': path[i] || 'search/1.json',
+				'content': JSON.stringify(search)
+			}))
+		}
+		return result
 	})
 }
